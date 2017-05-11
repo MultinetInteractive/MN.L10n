4f9b1e3719d429a77413c6659e6cbdb0db604b29
Chris Gårdenberg <chga@multinet.se>
2017-05-11 10:45:20 +0200

    Custom Web View för MVC

---
+++ MN.L10n/Mvc/L10nWebView.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace MN.L10n.Mvc
+{
+	public class L10nWebView<T> : WebViewPage<T>
+	{
+		public string _s(string phrase, object args = null) => L10n._s(phrase, args);
+
+		public string _m(string phrase, object args = null) => L10n._m(phrase, args);
+
+		public override void Execute()
+		{
+			
+		}
+	}
+}
