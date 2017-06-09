63e0e94f1bdcac5c6637ee9b97de1386a6213c09
Chris Gårdenberg <chga@multinet.se>
2017-06-09 15:48:44 +0200

    Lagt till constraint så att vi måste bygga med Debug för att den ska bry sig om fraserna. På det sättet slipper byggservern bry sig.

--- MN.L10n/CodeCompilerModule.cs
+++ MN.L10n/CodeCompilerModule.cs
@@ -8,7 +8,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace MN.L10n
 {
@@ -17,8 +16,12 @@ namespace MN.L10n
 		public L10n PhraseInstance { get; set; }
 		public void AfterCompile(AfterCompileContext context) { }
 
+		public static Regex r = new Regex(@"(?:MN\.)?(?:L10n\.)?(?:L10n\.)?_[sm]\(['""](.*)['""](?:,)?(.*?)\)", RegexOptions.Compiled);
+
 		public void BeforeCompile(BeforeCompileContext context)
 		{
+			if (context.Arguments.CompilationOptions.OptimizationLevel == OptimizationLevel.Release) return;
+
 			var baseDir = new DirectoryInfo(context.Arguments.BaseDirectory);
 
 			while (!baseDir.GetFiles("*.sln").Any())
@@ -60,49 +63,43 @@ namespace MN.L10n
 
 			var jsp = new JSParser();
 			var set = new CodeSettings { IgnoreAllErrors = false, MinifyCode = false, OutputMode = OutputMode.MultipleLines, BlocksStartOnSameLine = BlockStart.UseSource };
-
-			var r = new Regex(@"(?:MN\.)?(?:L10n\.)?(?:L10n\.)?_[sm]\(['""](.*)['""](?:,)?(.*?)\)", RegexOptions.Compiled);
+			
 			foreach(var file in fileList)
 			{
 				if (file.EndsWith(".js"))
 				{
-					var m = r.Matches(File.ReadAllText(file));
-					if (m.Count > 0)
+					foreach(var lang in PhraseInstance.Languages)
 					{
-
-						foreach(var lang in PhraseInstance.Languages)
+						var jsRewriter = new JSL10nTreeVisitor(PhraseInstance, lang);
+						var origSource = File.ReadAllText(file);
+						var astBlock = jsp.Parse(origSource, set);
+						jsRewriter.Visit(astBlock);
+						StringBuilder _code = new StringBuilder();
+						using (StringWriter sw = new StringWriter(_code))
 						{
-							var jsRewriter = new JSL10nTreeVisitor(PhraseInstance, lang);
-							var origSource = File.ReadAllText(file);
-							var astBlock = jsp.Parse(origSource, set);
-							jsRewriter.Visit(astBlock);
-							StringBuilder _code = new StringBuilder();
-							using (StringWriter sw = new StringWriter(_code))
-							{
-								OutputVisitor.Apply(sw, astBlock, set);
-							}
-							var code = _code.ToString();
-							if (code != origSource)
-							{
-								var jsExt = file.LastIndexOf(".js");
+							OutputVisitor.Apply(sw, astBlock, set);
+						}
+						var code = _code.ToString();
+						if (code != origSource)
+						{
+							var jsExt = file.LastIndexOf(".js");
 
-								var newFileName = string.Format("{0}-{1}.js", file.Substring(0, jsExt), lang);
+							var newFileName = string.Format("{0}-{1}.js", file.Substring(0, jsExt), lang);
 
-								File.WriteAllText(newFileName, code);
-								foreach (var up in jsRewriter.unusedPhrases)
+							File.WriteAllText(newFileName, code);
+							foreach (var up in jsRewriter.unusedPhrases)
+							{
+								if (phraseRewriter.unusedPhrases.Contains(up))
 								{
-									if (phraseRewriter.unusedPhrases.Contains(up))
-									{
-										phraseRewriter.unusedPhrases.Remove(up);
-									}
+									phraseRewriter.unusedPhrases.Remove(up);
 								}
 							}
-						};
-						context.Diagnostics.Add(
-						Diagnostic.Create(
-							new DiagnosticDescriptor("L10n", "TEST", "Checked phrases in: " + file, "Translated", DiagnosticSeverity.Info, true),
-							Location.None));
-					}
+						}
+					};
+					context.Diagnostics.Add(
+					Diagnostic.Create(
+						new DiagnosticDescriptor("L10n", "TEST", "Checked phrases in: " + file, "Translated", DiagnosticSeverity.Info, true),
+						Location.None));
 				}
 				else
 				{
