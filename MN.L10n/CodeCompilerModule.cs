184b56eba950be52345aaffae7a0dd4e6386c46c
Chris Gårdenberg <chga@multinet.se>
2017-06-12 13:46:53 +0200

    Lagt till så att den bara bygger fraser vid Debug

--- MN.L10n/CodeCompilerModule.cs
+++ MN.L10n/CodeCompilerModule.cs
@@ -16,10 +16,12 @@ namespace MN.L10n
 		public L10n PhraseInstance { get; set; }
 		public void AfterCompile(AfterCompileContext context) { }
 
-		public static Regex r = new Regex(@"(?:MN\.)?(?:L10n\.)?(?:L10n\.)?_[sm]\(['""](.*)['""](?:,)?(.*?)\)", RegexOptions.Compiled);
-
 		public void BeforeCompile(BeforeCompileContext context)
 		{
+			var runPhrase = true;
+			try { runPhrase = context.Arguments.CompilationOptions.OptimizationLevel == OptimizationLevel.Debug; } catch { }
+			if (!runPhrase) return;
+
 			var baseDir = new DirectoryInfo(context.Arguments.BaseDirectory);
 
 			while (!baseDir.GetFiles("*.sln").Any())
@@ -30,7 +32,7 @@ namespace MN.L10n
 			var solutionDir = baseDir.FullName;
 
 			var bpEnvName = solutionDir + "__l10n_build";
-			
+
 			var bpIdentifier = Environment.GetEnvironmentVariable(bpEnvName, EnvironmentVariableTarget.Machine);
 			if (string.IsNullOrWhiteSpace(bpIdentifier) || !IsValidBuildIdentifier(bpIdentifier))
 			{
@@ -42,14 +44,14 @@ namespace MN.L10n
 						Diagnostic.Create(
 							new DiagnosticDescriptor("L10n", "TEST", "BuildIdentifier: " + bpIdentifier, "Translated", DiagnosticSeverity.Info, true),
 							Location.None));
-			
+
 			PhraseInstance = L10n.CreateInstance(new NullLanguageProvider("1"), new FileDataProvider(solutionDir));
 
 			var validExtensions = new[] { ".aspx", ".ascx", ".js", ".jsx" };
 			var fileList = Directory.EnumerateFiles(context.Arguments.BaseDirectory, "*.*", SearchOption.AllDirectories)
 			.Where(f => validExtensions.Any(ext => f.EndsWith(ext, StringComparison.OrdinalIgnoreCase)));
 
-			var methods = new[] 
+			var methods = new[]
 			{
 				"_s",
 				"_m",
@@ -61,43 +63,49 @@ namespace MN.L10n
 
 			var jsp = new JSParser();
 			var set = new CodeSettings { IgnoreAllErrors = false, MinifyCode = false, OutputMode = OutputMode.MultipleLines, BlocksStartOnSameLine = BlockStart.UseSource };
-			
-			foreach(var file in fileList)
+
+			var r = new Regex(@"(?:MN\.)?(?:L10n\.)?(?:L10n\.)?_[sm]\(['""](.*)['""](?:,)?(.*?)\)", RegexOptions.Compiled);
+			foreach (var file in fileList)
 			{
 				if (file.EndsWith(".js"))
 				{
-					foreach(var lang in PhraseInstance.Languages)
+					var m = r.Matches(File.ReadAllText(file));
+					if (m.Count > 0)
 					{
-						var jsRewriter = new JSL10nTreeVisitor(PhraseInstance, lang);
-						var origSource = File.ReadAllText(file);
-						var astBlock = jsp.Parse(origSource, set);
-						jsRewriter.Visit(astBlock);
-						StringBuilder _code = new StringBuilder();
-						using (StringWriter sw = new StringWriter(_code))
-						{
-							OutputVisitor.Apply(sw, astBlock, set);
-						}
-						var code = _code.ToString();
-						if (code != origSource)
-						{
-							var jsExt = file.LastIndexOf(".js");
 
-							var newFileName = string.Format("{0}-{1}.js", file.Substring(0, jsExt), lang);
-
-							File.WriteAllText(newFileName, code);
-							foreach (var up in jsRewriter.unusedPhrases)
+						foreach (var lang in PhraseInstance.Languages)
+						{
+							var jsRewriter = new JSL10nTreeVisitor(PhraseInstance, lang);
+							var origSource = File.ReadAllText(file);
+							var astBlock = jsp.Parse(origSource, set);
+							jsRewriter.Visit(astBlock);
+							StringBuilder _code = new StringBuilder();
+							using (StringWriter sw = new StringWriter(_code))
 							{
-								if (phraseRewriter.unusedPhrases.Contains(up))
+								OutputVisitor.Apply(sw, astBlock, set);
+							}
+							var code = _code.ToString();
+							if (code != origSource)
+							{
+								var jsExt = file.LastIndexOf(".js");
+
+								var newFileName = string.Format("{0}-{1}.js", file.Substring(0, jsExt), lang);
+
+								File.WriteAllText(newFileName, code);
+								foreach (var up in jsRewriter.unusedPhrases)
 								{
-									phraseRewriter.unusedPhrases.Remove(up);
+									if (phraseRewriter.unusedPhrases.Contains(up))
+									{
+										phraseRewriter.unusedPhrases.Remove(up);
+									}
 								}
 							}
-						}
-					};
-					context.Diagnostics.Add(
-					Diagnostic.Create(
-						new DiagnosticDescriptor("L10n", "TEST", "Checked phrases in: " + file, "Translated", DiagnosticSeverity.Info, true),
-						Location.None));
+						};
+						context.Diagnostics.Add(
+						Diagnostic.Create(
+							new DiagnosticDescriptor("L10n", "TEST", "Checked phrases in: " + file, "Translated", DiagnosticSeverity.Info, true),
+							Location.None));
+					}
 				}
 				else
 				{
@@ -127,7 +135,7 @@ namespace MN.L10n
 				}
 			};
 
-			foreach(var st in context.Compilation.SyntaxTrees)
+			foreach (var st in context.Compilation.SyntaxTrees)
 			{
 				phraseRewriter._Class.Clear();
 				var model = context.Compilation.GetSemanticModel(st);
