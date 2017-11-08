d789696c45f7e4bbd749a1265855153172d0a42f
lice <lice@multinet.se>
2017-11-08 08:39:39 +0100

    Added a different function for CreateJavascriptEvaluator which allows you to provide the language explicitly

--- MN.L10n.Javascript/RuleEvaluatorFactory.cs
+++ MN.L10n.Javascript/RuleEvaluatorFactory.cs
@@ -8,17 +8,32 @@ namespace MN.L10n.Javascript
 {
     public class RuleEvaluatorFactory
     {
-        public static string CreateJavascriptRuleEvaluator(System.Web.Caching.Cache cache)
+        public static string CreateJavascriptRuleEvaluator(string language, System.Web.Caching.Cache cache)
         {
-            var language = L10n.GetLanguage();
-            L10nLanguage l10nItem = cache["__l10n_" + language] as L10nLanguage;
+            L10nLanguage l10nItem = null;
+            if (cache != null)
+            {
+                l10nItem = cache["__l10n_" + language] as L10nLanguage;
+            }
+
             if (l10nItem == null)
             {
                 l10nItem = L10n.GetL10nLanguage(language);
-                cache["__l10n_" + language] = l10nItem;
+
+                if (cache != null)
+                {
+                    cache["__l10n_" + language] = l10nItem;
+                }
             }
+
             return "window.l10n = " + Jil.JSON.Serialize(l10nItem, Jil.Options.ISO8601PrettyPrint) + ";" +
                    "window.l10n.ruleEvaluator = function(n) { return ~~(" + l10nItem.PluralRule + "); };";
+        }
+
+        public static string CreateJavascriptRuleEvaluator(System.Web.Caching.Cache cache)
+        {
+            var language = L10n.GetLanguage();
+            return CreateJavascriptRuleEvaluator(language, cache);
         }
     }
 }
