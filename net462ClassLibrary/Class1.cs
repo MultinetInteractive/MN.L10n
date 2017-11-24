0e05641d4a378a388743684b796fc3b261f82e99
Chris Gårdenberg <chga@multinet.se>
2017-11-24 14:04:20 +0100

    Buildtask as Exec instead of normal one..

--- net462ClassLibrary/Class1.cs
+++ net462ClassLibrary/Class1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static MN.L10n.L10n;
 
 namespace net462ClassLibrary
 {
     public class Class1
     {
+		public string TestString = _s("Hello World");
     }
 }
