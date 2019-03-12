794b4224c50a078ffc770ac5b9266bca3e9408a4
Chris Gårdenberg <chga@multinet.se>
2019-03-12 17:10:21 +0100

    Made so the buildtask would throw errors into the event viewer for easier debugging

--- MN.L10n.BuildTasks/Program.cs
+++ MN.L10n.BuildTasks/Program.cs
@@ -1,5 +1,6 @@
 ﻿using MN.L10n.FileProviders;
 using MN.L10n.NullProviders;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -7,283 +8,297 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
-using Newtonsoft.Json;
 
 namespace MN.L10n.BuildTasks
 {
-	class Program
-	{
+    class Program
+    {
         static int Main(string[] args)
-		{
-			string projectFolder = string.Empty;
-			if (args.Length == 0)
-			{
-				throw new ArgumentNullException("projectFolder");
-			}
-			else
-			{
-				projectFolder = string.Join(" ", args[0]).TrimEnd(Path.DirectorySeparatorChar, '"');
-			}
+        {
+            string projectFolder = string.Empty;
+            if (args.Length == 0)
+            {
+                throw new ArgumentNullException("projectFolder");
+            }
+            else
+            {
+                projectFolder = string.Join(" ", args[0]).TrimEnd(Path.DirectorySeparatorChar, '"');
+            }
 
-			Stopwatch stw = new Stopwatch();
-			var fi = new DirectoryInfo(projectFolder);
-			var baseDir = fi;
-			var sourceDir = Environment.CurrentDirectory;
+            Stopwatch stw = new Stopwatch();
+            var fi = new DirectoryInfo(projectFolder);
+            var baseDir = fi;
+            var sourceDir = Environment.CurrentDirectory;
 
-			Console.WriteLine(baseDir.FullName + ";");
-			Console.WriteLine("info l10n: L10n - beginning work: " + sourceDir);
+            Console.WriteLine(baseDir.FullName + ";");
+            Console.WriteLine("info l10n: L10n - beginning work: " + sourceDir);
 
-			stw.Start();
+            stw.Start();
 
-			while (!baseDir.EnumerateFiles().Any(x => x.Extension == ".sln" || x.Extension == ".l10nroot"))
-			{
-				baseDir = baseDir.Parent;
-			}
+            while (!baseDir.EnumerateFiles().Any(x => x.Extension == ".sln" || x.Extension == ".l10nroot"))
+            {
+                baseDir = baseDir.Parent;
+            }
 
-			var solutionDir = baseDir.FullName;
+            var solutionDir = baseDir.FullName;
             var config = new L10nConfig();
 
-		    var cfgFile = baseDir.GetFiles(".l10nconfig").FirstOrDefault();
-		    if (cfgFile != null)
-		    {
-		        try
-		        {
-		            config = JsonConvert.DeserializeObject<L10nConfig>(File.ReadAllText(cfgFile.FullName));
-		        }
-		        catch (Exception ex)
-		        {
-		            Console.WriteLine("Unable to read L10n config file : " + ex.ToString());
-		            return -1;
-		        }
-		    }
+            var cfgFile = baseDir.GetFiles(".l10nconfig").FirstOrDefault();
+            if (cfgFile != null)
+            {
+                try
+                {
+                    config = JsonConvert.DeserializeObject<L10nConfig>(File.ReadAllText(cfgFile.FullName));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unable to read L10n config file : " + ex.ToString());
+                    return -1;
+                }
+            }
 
-		    if (config != null && config.PreventBuildTask)
-		    {
-		        Console.WriteLine("info l10n: L10n build task cancelled by config file");
-		        return 0;
-		    }
+            if (config != null && config.PreventBuildTask)
+            {
+                Console.WriteLine("info l10n: L10n build task cancelled by config file");
+                return 0;
+            }
 
             var lockFile = Path.Combine(solutionDir, ".l10nLock");
-			if (CheckForLockFile(lockFile, projectFolder, baseDir, config) == 0)
-			{
-				return 0;
-			}
+            if (CheckForLockFile(lockFile, projectFolder, baseDir, config) == 0)
+            {
+                return 0;
+            }
 
             try
-			{
-				if (config != null && config.PreventBuildTask)
-				{
-					Console.WriteLine("info l10n: L10n build task cancelled by config file");
-					File.Delete(lockFile);
-					return 0;
-				}
+            {
+                if (config != null && config.PreventBuildTask)
+                {
+                    Console.WriteLine("info l10n: L10n build task cancelled by config file");
+                    File.Delete(lockFile);
+                    return 0;
+                }
 
-				L10n PhraseInstance = L10n.CreateInstance(
-					new NullLanguageProvider(),
-					new FileDataProvider(solutionDir)
-				);
+                L10n PhraseInstance = L10n.CreateInstance(
+                    new NullLanguageProvider(),
+                    new FileDataProvider(solutionDir)
+                );
 
-				var validExtensions = new[] { ".aspx", ".ascx", ".js", ".jsx", ".cs", ".cshtml", ".ts", ".tsx", ".master", ".ashx", ".php" };
+                var validExtensions = new[] { ".aspx", ".ascx", ".js", ".jsx", ".cs", ".cshtml", ".ts", ".tsx", ".master", ".ashx", ".php" };
 
-				var defaultIgnorePaths = new[] {
-					"/.git/", "\\.git\\",
-					"/node_modules/", "\\node_modules\\",
-					"/.vscode/", "\\.vscode",
-					"/.idea/", "\\.idea",
-					"/.vs/", "\\.vs",
-					"/bin/", "\\bin",
-					"/obj/", "\\obj\\",
-					"/packages/", "\\packages\\",
-				};
+                var defaultIgnorePaths = new[] {
+                    "/.git/", "\\.git\\",
+                    "/node_modules/", "\\node_modules\\",
+                    "/.vscode/", "\\.vscode",
+                    "/.idea/", "\\.idea",
+                    "/.vs/", "\\.vs",
+                    "/bin/", "\\bin",
+                    "/obj/", "\\obj\\",
+                    "/packages/", "\\packages\\",
+                };
 
-				var defaultIgnoreExtensions = new[] {
-					".min.js", ".css",
-					".dll", ".designer.cs",
-				};
+                var defaultIgnoreExtensions = new[] {
+                    ".min.js", ".css",
+                    ".dll", ".designer.cs",
+                };
 
-				List<string> fileList = new List<string>();
+                List<string> fileList = new List<string>();
 
-				if (config != null)
-				{
-					if (config.IncludePatterns.Count == 0)
-					{
-						fileList = Directory.EnumerateFiles(solutionDir, "*.*", SearchOption.AllDirectories)
-						.Where(f => validExtensions.Any(ext => f.EndsWith(ext, StringComparison.OrdinalIgnoreCase)))
-						.Where(f =>
-							!defaultIgnoreExtensions.Any(ign => f.ToLower().EndsWith(ign)) &&
-							!defaultIgnorePaths.Any(ign => f.ToLower().Contains(ign))
-						)
-						.ToList();
-					}
-					else
-					{
-						var fileListWithIgnores = Directory.EnumerateFiles(solutionDir, "*.*", SearchOption.AllDirectories)
-							.Where(f => !defaultIgnorePaths.Any(ign => f.ToLower().Contains(ign)));
+                if (config != null)
+                {
+                    if (config.IncludePatterns.Count == 0)
+                    {
+                        fileList = Directory.EnumerateFiles(solutionDir, "*.*", SearchOption.AllDirectories)
+                        .Where(f => validExtensions.Any(ext => f.EndsWith(ext, StringComparison.OrdinalIgnoreCase)))
+                        .Where(f =>
+                            !defaultIgnoreExtensions.Any(ign => f.ToLower().EndsWith(ign)) &&
+                            !defaultIgnorePaths.Any(ign => f.ToLower().Contains(ign))
+                        )
+                        .ToList();
+                    }
+                    else
+                    {
+                        var fileListWithIgnores = Directory.EnumerateFiles(solutionDir, "*.*", SearchOption.AllDirectories)
+                            .Where(f => !defaultIgnorePaths.Any(ign => f.ToLower().Contains(ign)));
 
-						var filesListWithIncludes = Directory.EnumerateFiles(solutionDir, "*.*", SearchOption.AllDirectories)
-							.Where(f => config.IncludePatterns.Any(p => f.ToLower().Contains(p.ToLower())));
+                        var filesListWithIncludes = Directory.EnumerateFiles(solutionDir, "*.*", SearchOption.AllDirectories)
+                            .Where(f => config.IncludePatterns.Any(p => f.ToLower().Contains(p.ToLower())));
 
-						var combinedList = fileListWithIgnores.Union(filesListWithIncludes).Where(f => validExtensions.Any(ext => f.EndsWith(ext, StringComparison.OrdinalIgnoreCase)));
+                        var combinedList = fileListWithIgnores.Union(filesListWithIncludes).Where(f => validExtensions.Any(ext => f.EndsWith(ext, StringComparison.OrdinalIgnoreCase)));
 
-						fileList.AddRange(combinedList);
-					}
+                        fileList.AddRange(combinedList);
+                    }
 
 
-					if (config.ExcludePatterns.Count > 0)
-					{
-						fileList = fileList.Where(f => config.ExcludePatterns.All(p => !f.ToLower().Contains(p.ToLower()))).ToList();
-					}
-				}
-				else
-				{
-					fileList = Directory.EnumerateFiles(solutionDir, "*.*", SearchOption.AllDirectories)
-					.Where(f => validExtensions.Any(ext => f.EndsWith(ext, StringComparison.OrdinalIgnoreCase)))
-					.Where(f => !defaultIgnorePaths.Any(ign => f.ToLower().Contains(ign)))
-					.ToList();
-				}
+                    if (config.ExcludePatterns.Count > 0)
+                    {
+                        fileList = fileList.Where(f => config.ExcludePatterns.All(p => !f.ToLower().Contains(p.ToLower()))).ToList();
+                    }
+                }
+                else
+                {
+                    fileList = Directory.EnumerateFiles(solutionDir, "*.*", SearchOption.AllDirectories)
+                    .Where(f => validExtensions.Any(ext => f.EndsWith(ext, StringComparison.OrdinalIgnoreCase)))
+                    .Where(f => !defaultIgnorePaths.Any(ign => f.ToLower().Contains(ign)))
+                    .ToList();
+                }
 
-				var phraseRewriter = new PhrasesRewriter(PhraseInstance);
+                var phraseRewriter = new PhrasesRewriter(PhraseInstance);
 
-				var parser = new L10nParser();
+                var parser = new L10nParser();
 
-				foreach (var p in PhraseInstance.Phrases)
-				{
-					p.Value.Sources = new List<string>();
-				}
+                foreach (var p in PhraseInstance.Phrases)
+                {
+                    p.Value.Sources = new List<string>();
+                }
 
-				foreach (var file in fileList.Distinct())
-				{
-					var fileContents = File.ReadAllText(file);
-					var shortFile = file.Replace(solutionDir, "");
-					var invocations = parser.Parse(fileContents);
+                foreach (var file in fileList.Distinct())
+                {
+                    var fileContents = File.ReadAllText(file);
+                    var shortFile = file.Replace(solutionDir, "");
+                    var invocations = parser.Parse(fileContents);
 
-					foreach (var _phrase in invocations)
-					{
-						if (!PhraseInstance.Phrases.ContainsKey(_phrase.Phrase))
-						{
-							PhraseInstance.Phrases.TryAdd(_phrase.Phrase, new L10nPhrase() { Sources = new List<string> { shortFile + ":" + _phrase.Row } });
-						}
-						else
-						{
-							PhraseInstance.Phrases[_phrase.Phrase].Usages++;
-							if (!PhraseInstance.Phrases[_phrase.Phrase].Sources.Contains(shortFile + ":" + _phrase.Row))
-							{
-								PhraseInstance.Phrases[_phrase.Phrase].Sources.Add(shortFile + ":" + _phrase.Row);
-							}
-						}
+                    foreach (var _phrase in invocations)
+                    {
+                        if (!PhraseInstance.Phrases.ContainsKey(_phrase.Phrase))
+                        {
+                            PhraseInstance.Phrases.TryAdd(_phrase.Phrase, new L10nPhrase() { Sources = new List<string> { shortFile + ":" + _phrase.Row } });
+                        }
+                        else
+                        {
+                            PhraseInstance.Phrases[_phrase.Phrase].Usages++;
+                            if (!PhraseInstance.Phrases[_phrase.Phrase].Sources.Contains(shortFile + ":" + _phrase.Row))
+                            {
+                                PhraseInstance.Phrases[_phrase.Phrase].Sources.Add(shortFile + ":" + _phrase.Row);
+                            }
+                        }
 
-						if (phraseRewriter.unusedPhrases.Contains(_phrase.Phrase))
-						{
-							phraseRewriter.unusedPhrases.Remove(_phrase.Phrase);
-						}
-					}
-					if (config.ShowDetailedLog) Console.WriteLine("info l10n: Checked phrases in: " + shortFile + ", found " + invocations.Count + " phrases");
-				};
+                        if (phraseRewriter.unusedPhrases.Contains(_phrase.Phrase))
+                        {
+                            phraseRewriter.unusedPhrases.Remove(_phrase.Phrase);
+                        }
+                    }
+                    if (config.ShowDetailedLog) Console.WriteLine("info l10n: Checked phrases in: " + shortFile + ", found " + invocations.Count + " phrases");
+                };
 
-				phraseRewriter.SavePhrasesToFile();
-				stw.Stop();
-				Console.WriteLine("info l10n: Spent " + stw.Elapsed + " running L10n, found " + PhraseInstance.Phrases.Count + " phrases");
+                phraseRewriter.SavePhrasesToFile();
+                stw.Stop();
+                Console.WriteLine("info l10n: Spent " + stw.Elapsed + " running L10n, found " + PhraseInstance.Phrases.Count + " phrases");
 
-				MovePhraseFiles(projectFolder, baseDir, config);
+                MovePhraseFiles(projectFolder, baseDir, config);
 
-				File.Delete(lockFile);
-				return 0;
-			}
-			catch(Exception ex)
-			{
-				File.Delete(lockFile);
-				Console.WriteLine(ex.ToString());
-				return -1;
-			}
-			finally
-			{
-				File.Delete(lockFile);
-			}
-		}
+                if (File.Exists(lockFile))
+                {
+                    File.Delete(lockFile);
+                }
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                using (EventLog el = new EventLog("Application"))
+                {
+                    el.Source = "MN.L10n.BuildTasks";
+                    el.WriteEntry(ex.ToString(), EventLogEntryType.Error);
+                }
 
-		private static void MovePhraseFiles(string projectFolder, DirectoryInfo baseDir, L10nConfig config)
-		{
-			var dir = new DirectoryInfo(baseDir.FullName);
+                if (File.Exists(lockFile))
+                {
+                    File.Delete(lockFile);
+                }
+                return -1;
+            }
+            finally
+            {
+                if (File.Exists(lockFile))
+                {
+                    File.Delete(lockFile);
+                }
+            }
+        }
 
-			var files = dir.GetFiles();
+        private static void MovePhraseFiles(string projectFolder, DirectoryInfo baseDir, L10nConfig config)
+        {
+            var dir = new DirectoryInfo(baseDir.FullName);
 
-			var langRegex = new Regex("language-[^\\.]*\\.json");
+            var files = dir.GetFiles();
 
-			var toCopy = new List<FileInfo>();
-			foreach (var file in files)
-			{
-				if (file.Name == "phrases.json" || file.Name == "languages.json" || langRegex.IsMatch(file.Name))
-				{
-					toCopy.Add(file);
-				}
-			}
+            var langRegex = new Regex("language-[^\\.]*\\.json");
 
-		    var copyTo = config.CopyFilesTo?.Count > 0 ? config.CopyFilesTo.Select(to => Path.Combine(baseDir.FullName, to, "L10n"))
+            var toCopy = new List<FileInfo>();
+            foreach (var file in files)
+            {
+                if (file.Name == "phrases.json" || file.Name == "languages.json" || langRegex.IsMatch(file.Name))
+                {
+                    toCopy.Add(file);
+                }
+            }
+
+            var copyTo = config.CopyFilesTo?.Count > 0 ? config.CopyFilesTo.Select(to => Path.Combine(baseDir.FullName, to, "L10n"))
                 : new List<string>
-		    {
-		        Path.Combine(projectFolder, "L10n")
-		    };
+            {
+                Path.Combine(projectFolder, "L10n")
+            };
 
-		    foreach (var destDirName in copyTo)
-		    {
-		        if (!Directory.Exists(destDirName))
-		        {
-		            Directory.CreateDirectory(destDirName);
-		        }
+            foreach (var destDirName in copyTo)
+            {
+                if (!Directory.Exists(destDirName))
+                {
+                    Directory.CreateDirectory(destDirName);
+                }
 
-		        Console.WriteLine($@"Copying phrase-files from {baseDir.FullName} to {destDirName}");
+                Console.WriteLine($@"Copying phrase-files from {baseDir.FullName} to {destDirName}");
 
-		        foreach (var file in toCopy)
-		        {
-		            file.CopyTo(Path.Combine(destDirName, file.Name), true);
-		        }
+                foreach (var file in toCopy)
+                {
+                    file.CopyTo(Path.Combine(destDirName, file.Name), true);
+                }
 
-		        Console.WriteLine($@"Files copied to {destDirName}");
-		    }
-		}
+                Console.WriteLine($@"Files copied to {destDirName}");
+            }
+        }
 
-		private static int CheckForLockFile(string lockFile, string projectFolder, DirectoryInfo baseDir, L10nConfig config)
-		{
-			var lockFileExists = File.Exists(lockFile);
-			if (lockFileExists)
-			{
+        private static int CheckForLockFile(string lockFile, string projectFolder, DirectoryInfo baseDir, L10nConfig config)
+        {
+            var lockFileExists = File.Exists(lockFile);
+            if (lockFileExists)
+            {
 
-				Console.WriteLine("info l10n: Lock file exists, waiting until it's gone");
-				while (lockFileExists)
-				{
-					var lockContents = File.ReadAllText(lockFile);
+                Console.WriteLine("info l10n: Lock file exists, waiting until it's gone");
+                while (lockFileExists)
+                {
+                    var lockContents = File.ReadAllText(lockFile);
 
-					var utcNow = DateTime.UtcNow;
-					if (!DateTime.TryParse(lockContents, out DateTime lockTime))
-					{
-						Console.WriteLine("warn l10n: Corrupted lock file detected, removing");
-						File.Delete(lockFile);
-					}
+                    var utcNow = DateTime.UtcNow;
+                    if (!DateTime.TryParse(lockContents, out DateTime lockTime))
+                    {
+                        Console.WriteLine("warn l10n: Corrupted lock file detected, removing");
+                        File.Delete(lockFile);
+                    }
 
-					if (Math.Abs((utcNow - lockTime).TotalSeconds) > 30)
-					{
-						Console.WriteLine("warn l10n: Build took over 30 seconds, removing lock file");
-						File.Delete(lockFile);
-					}
+                    if (Math.Abs((utcNow - lockTime).TotalSeconds) > 30)
+                    {
+                        Console.WriteLine("warn l10n: Build took over 30 seconds, removing lock file");
+                        File.Delete(lockFile);
+                    }
 
-					Thread.Sleep(500);
-					lockFileExists = File.Exists(lockFile);
-				}
+                    Thread.Sleep(500);
+                    lockFileExists = File.Exists(lockFile);
+                }
 
-				MovePhraseFiles(projectFolder, baseDir, config);
-				return 0;
-			}
+                MovePhraseFiles(projectFolder, baseDir, config);
+                return 0;
+            }
 
-			try
-			{
-				File.WriteAllText(lockFile, DateTime.UtcNow.ToString());
-			}
-			catch
-			{
-				return CheckForLockFile(lockFile, projectFolder, baseDir, config);
-			}
+            try
+            {
+                File.WriteAllText(lockFile, DateTime.UtcNow.ToString());
+            }
+            catch
+            {
+                return CheckForLockFile(lockFile, projectFolder, baseDir, config);
+            }
 
-			return -1;
-		}
-	}
+            return -1;
+        }
+    }
 }
