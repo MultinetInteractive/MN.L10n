a68ebe3d1c065086532483bb3ecec102be498efd
Chris Gårdenberg <chga@multinet.se>
2019-10-22 11:33:05 +0200

    Managed to get the VSIX installable on VS2019

--- MN.L10n.Analyzer/MN.L10n.Analyzer.Test/MNL10nAnalyzerUnitTests.cs
+++ MN.L10n.Analyzer/MN.L10n.Analyzer.Test/MNL10nAnalyzerUnitTests.cs
@@ -1,82 +1,158 @@
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+using System.Collections.Generic;
 using TestHelper;
-using MN.L10n.Analyzer;
 
 namespace MN.L10n.Analyzer.Test
 {
-	[TestClass]
-	public class UnitTest : CodeFixVerifier
-	{
+    [TestClass]
+    public class UnitTest : CodeFixVerifier
+    {
 
-		//No diagnostics expected to show up
-		[TestMethod]
-		public void TestMethod1()
-		{
-			var test = @"";
+        //No diagnostics expected to show up
+        [TestMethod]
+        public void Test_MN0003_DoNotAllowWhiteSpaceAtStartOrEndOfString()
+        {
+            var test = @"
+          using System;
+          using System.Collections.Generic;
+          using System.Linq;
+          using System.Text;
+          using System.Threading.Tasks;
+          using System.Diagnostics;
 
-			VerifyCSharpDiagnostic(test);
-		}
+          namespace ConsoleApplication1
+          {
+              class TypeName
+              {   
+                public void Main() {
+                    _s(""Testing "");
+                    _s("" Testing"");
+                }
+              }
+          }";
 
-		////Diagnostic and CodeFix both triggered and checked for
-		//[TestMethod]
-		//public void TestMethod2()
-		//{
-		//	var test = @"
-  //  using System;
-  //  using System.Collections.Generic;
-  //  using System.Linq;
-  //  using System.Text;
-  //  using System.Threading.Tasks;
-  //  using System.Diagnostics;
+            var expectations = new List<DiagnosticResult>();
+            expectations.Add(new DiagnosticResult
+            {
+                Id = "MN0003",
+                Message = "The string cannot start or end with whitespaces.",
+                Severity = DiagnosticSeverity.Error,
+                Locations = new[] {
+                    new DiagnosticResultLocation("Test0.cs", 14, 21)
+                }
+            });
 
-  //  namespace ConsoleApplication1
-  //  {
-  //      class TypeName
-  //      {   
-  //      }
-  //  }";
-		//	var expected = new DiagnosticResult
-		//	{
-		//		Id = "MNL10nAnalyzer",
-		//		Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
-		//		Severity = DiagnosticSeverity.Warning,
-		//		Locations =
-		//			new[] {
-		//					new DiagnosticResultLocation("Test0.cs", 11, 15)
-		//				}
-		//	};
+            expectations.Add(new DiagnosticResult
+            {
+                Id = "MN0003",
+                Message = "The string cannot start or end with whitespaces.",
+                Severity = DiagnosticSeverity.Error,
+                Locations = new[] {
+                    new DiagnosticResultLocation("Test0.cs", 15, 21)
+                }
+            });
 
-		//	VerifyCSharpDiagnostic(test, expected);
+            VerifyCSharpDiagnostic(test, expectations.ToArray());
+        }
 
-		//	var fixtest = @"
-  //  using System;
-  //  using System.Collections.Generic;
-  //  using System.Linq;
-  //  using System.Text;
-  //  using System.Threading.Tasks;
-  //  using System.Diagnostics;
+        //No diagnostics expected to show up
+        [TestMethod]
+        public void Test_MN0007_DoNotAllowNonClassAsKeywordArgument()
+        {
+            var test = @"
+          using System;
+          using System.Collections.Generic;
+          using System.Linq;
+          using System.Text;
+          using System.Threading.Tasks;
+          using System.Diagnostics;
 
-  //  namespace ConsoleApplication1
-  //  {
-  //      class TYPENAME
-  //      {   
-  //      }
-  //  }";
-		//	VerifyCSharpFix(test, fixtest);
-		//}
+          namespace ConsoleApplication1
+          {
+              class TestClass { public string someParameter { get; set; } = ""I am the testiest of strings""; }
+              class TypeName
+              {   
+                public void Main() {
+                    _s(""Testing $someParameter$"", ""I am a test string"");
+                    _s(""Testing $someParameter$"", new { someParameter = ""I am a test string"" });
+                    _s(""Testing $someParameter$"", new TestClass());
+                }
+              }
+          }";
 
-		protected override CodeFixProvider GetCSharpCodeFixProvider()
-		{
-			return new MNL10nCodeFixProvider();
-		}
+            var expectations = new List<DiagnosticResult>();
+            expectations.Add(new DiagnosticResult
+            {
+                Id = "MN0007",
+                Message = "L10n requires a class or anonymous type for keywords.",
+                Severity = DiagnosticSeverity.Error,
+                Locations = new[] {
+                    new DiagnosticResultLocation("Test0.cs", 15, 21)
+                }
+            });
 
-		protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
-		{
-			return new MNL10nAnalyzer();
-		}
-	}
+            VerifyCSharpDiagnostic(test, expectations.ToArray());
+        }
+
+        ////Diagnostic and CodeFix both triggered and checked for
+        //[TestMethod]
+        //public void TestMethod2()
+        //{
+        //	var test = @"
+        //  using System;
+        //  using System.Collections.Generic;
+        //  using System.Linq;
+        //  using System.Text;
+        //  using System.Threading.Tasks;
+        //  using System.Diagnostics;
+
+        //  namespace ConsoleApplication1
+        //  {
+        //      class TypeName
+        //      {   
+        //      }
+        //  }";
+        //	var expected = new DiagnosticResult
+        //	{
+        //		Id = "MNL10nAnalyzer",
+        //		Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
+        //		Severity = DiagnosticSeverity.Warning,
+        //		Locations =
+        //			new[] {
+        //					new DiagnosticResultLocation("Test0.cs", 11, 15)
+        //				}
+        //	};
+
+        //	VerifyCSharpDiagnostic(test, expected);
+
+        //	var fixtest = @"
+        //  using System;
+        //  using System.Collections.Generic;
+        //  using System.Linq;
+        //  using System.Text;
+        //  using System.Threading.Tasks;
+        //  using System.Diagnostics;
+
+        //  namespace ConsoleApplication1
+        //  {
+        //      class TYPENAME
+        //      {   
+        //      }
+        //  }";
+        //	VerifyCSharpFix(test, fixtest);
+        //}
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new MNL10nCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new MNL10nAnalyzer();
+        }
+    }
 }
--- MN.L10n.Analyzer/MN.L10n.Analyzer/MNL10nAnalyzer.cs
+++ MN.L10n.Analyzer/MN.L10n.Analyzer/MNL10nAnalyzer.cs
@@ -1,65 +1,64 @@
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Threading;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace MN.L10n.Analyzer
 {
-	[DiagnosticAnalyzer(LanguageNames.CSharp)]
-	public class MNL10nAnalyzer : DiagnosticAnalyzer
-	{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MNL10nAnalyzer : DiagnosticAnalyzer
+    {
         public static readonly DiagnosticDescriptor NoParamRule = new DiagnosticDescriptor("MN0001", "Missing arguments", "Need to send variables to '{0}'", "L10n", DiagnosticSeverity.Error, isEnabledByDefault: true);
         public static readonly DiagnosticDescriptor MemberAccessorRule = new DiagnosticDescriptor("MN0002", "Input is not a known string", "L10n can only evaluate string literals when finding used phrases. If the phrase is known you can ignore this, but you should only use L10n with known strings when possible.", "L10n", DiagnosticSeverity.Warning, isEnabledByDefault: true);
         public static readonly DiagnosticDescriptor NoWhitespaceAtStartOrEndRule = new DiagnosticDescriptor("MN0003", "String starts/ends with whitespace", "The string cannot start or end with whitespaces.", "L10n", DiagnosticSeverity.Error, isEnabledByDefault: true);
         public static readonly DiagnosticDescriptor NoEmptyStringsEndRule = new DiagnosticDescriptor("MN0004", "Input is an empty string", "The string cannot start or end with whitespace", "L10n", DiagnosticSeverity.Error, isEnabledByDefault: true);
         public static readonly DiagnosticDescriptor NoStringInterpolationRule = new DiagnosticDescriptor("MN0005", "Interpolated string used", "L10n can only evaluate string literals when finding used phrases. Never use string interpolation with L10n. It is not supported yet.", "L10n", DiagnosticSeverity.Error, isEnabledByDefault: true);
         public static readonly DiagnosticDescriptor NoStringConcatRule = new DiagnosticDescriptor("MN0006", "String concatenation used", "L10n can only evaluate a single string literal when finding used phrases. Never use string concatenation with L10n. It is not supported yet.", "L10n", DiagnosticSeverity.Error, isEnabledByDefault: true);
+        public static readonly DiagnosticDescriptor ArgumentsNotAnClass = new DiagnosticDescriptor("MN0007", "Invalid type for keywords", "L10n requires a class or anonymous type for keywords.", "L10n", DiagnosticSeverity.Error, isEnabledByDefault: true);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics {
             get {
                 return ImmutableArray.Create(
-                    NoParamRule, 
-                    MemberAccessorRule, 
-                    NoWhitespaceAtStartOrEndRule, 
-                    NoEmptyStringsEndRule, 
+                    NoParamRule,
+                    MemberAccessorRule,
+                    NoWhitespaceAtStartOrEndRule,
+                    NoEmptyStringsEndRule,
                     NoStringInterpolationRule,
-                    NoStringConcatRule
+                    NoStringConcatRule,
+                    ArgumentsNotAnClass
                     );
             }
         }
 
-		public override void Initialize(AnalysisContext context)
-		{
+        public override void Initialize(AnalysisContext context)
+        {
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
             context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.InvocationExpression);
-		}
+        }
 
         private static string[] validIdentifiers = new[]
         {
             "_s", "_sr", "_m", "_mr"
         };
 
-		private void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext obj)
-		{
+        private void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext obj)
+        {
             var ies = obj.Node as InvocationExpressionSyntax;
             if (ies == null) return;
-            
+
             var expr = ies.Expression as IdentifierNameSyntax;
             if (expr == null || !validIdentifiers.Contains(expr.Identifier.Text)) return;
 
             var arguments = ies.ArgumentList.Arguments;
-            if(arguments.Count == 0)
+            if (arguments.Count == 0)
                 obj.ReportDiagnostic(Diagnostic.Create(NoParamRule, obj.Node.GetLocation()));
             else
             {
                 var supposedString = arguments.First();
-                switch(supposedString.Expression)
+                switch (supposedString.Expression)
                 {
                     case BinaryExpressionSyntax binaryExpression:
                         obj.ReportDiagnostic(Diagnostic.Create(NoStringConcatRule, obj.Node.GetLocation()));
@@ -79,14 +78,24 @@ namespace MN.L10n.Analyzer
                         }
                         else
                         {
-                            if(char.IsWhiteSpace(text.First()) || char.IsWhiteSpace(text.Last()))
+                            if (char.IsWhiteSpace(text.First()) || char.IsWhiteSpace(text.Last()))
                             {
                                 obj.ReportDiagnostic(Diagnostic.Create(NoWhitespaceAtStartOrEndRule, obj.Node.GetLocation()));
                             }
                         }
                         break;
                 }
+
+                if (arguments.Count > 1)
+                {
+                    var supposedArgument = arguments[1];
+
+                    if (!(supposedArgument.Expression is ObjectCreationExpressionSyntax || supposedArgument.Expression is AnonymousObjectCreationExpressionSyntax))
+                    {
+                        obj.ReportDiagnostic(Diagnostic.Create(ArgumentsNotAnClass, obj.Node.GetLocation()));
+                    }
+                }
             }
-		}
-	}
+        }
+    }
 }
