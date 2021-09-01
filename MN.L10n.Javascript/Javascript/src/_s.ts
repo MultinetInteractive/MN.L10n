c0c5ec70939e95398cf305137418e2fa1fd57ac9
LinusCenterstrom <linuscent@gmail.com>
2021-09-01 11:40:59 +0200

    Added a more specific returntype from _s (#104)

--- MN.L10n.Javascript/Javascript/src/_s.ts
+++ MN.L10n.Javascript/Javascript/src/_s.ts
@@ -1,15 +1,15 @@
 import { getPhrase } from "./internal/GetPhrase";
 import { replaceKeywords } from "./internal/ReplaceKeywords";
-import { ExtractL10nParameter } from "./publicTypes";
+import { ExtractL10nParameter, TranslatedString } from "./publicTypes";
 
 export function _s<T extends string>(
   l10nString: T,
   formatParameters?: {
     [key in ExtractL10nParameter<T>]: string | number;
   }
-): string {
+): TranslatedString {
   return replaceKeywords(
     getPhrase(l10nString, formatParameters),
     formatParameters
-  );
+  ) as unknown as TranslatedString;
 }
