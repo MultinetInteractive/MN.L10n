cd6df21ee150e81042046498cb35c5da9ec3f44a
LinusCenterstrom <linuscent@gmail.com>
2021-09-01 10:52:50 +0200

    Added a more specific returntype from _s

--- MN.L10n.Javascript/Javascript/L10n.js
+++ MN.L10n.Javascript/Javascript/L10n.js
@@ -1,2 +1,76 @@
-(function(){function l(){var n=null;return typeof window!="undefined"?n=window:typeof global!="undefined"&&(n=global),n}function f(){var n=l();if(n==null)return null;var e=n.l10n;return typeof e=="undefined"?null:e}function u(n,e){typeof e=="undefined"&&(e={});var t=f();if(t==null)return n;var r=t.Phrases[n],i;return typeof e.__count!="undefined"&&(i=t.ruleEvaluator(e.__count).toString()),typeof r!="undefined"&&(typeof i!="undefined"&&typeof r.r[i]!="undefined"?n=r.r[i]:n=r.r["0"]),n}function d(n,e){var r,i;if(typeof e=="undefined")return n;for(var t in e)e.hasOwnProperty(t)&&(n=n.split("$"+t+"$").join((i=(r=e[t])==null?void 0:r.toString())!=null?i:""));return n}function o(n,e){return d(u(n,e),e)}typeof window!="undefined"&&(window._s=o);typeof global!="undefined"&&(global._s=o);})();
+(function() {
+  // src/internal/GetGlobal.ts
+  function getGlobal() {
+    var target = null;
+    if (typeof window !== "undefined") {
+      target = window;
+    } else if (typeof global !== "undefined") {
+      target = global;
+    }
+    return target;
+  }
+
+  // src/internal/GetL10n.ts
+  function getL10n() {
+    var global2 = getGlobal();
+    if (global2 == null) {
+      return null;
+    }
+    var l10n = global2.l10n;
+    if (typeof l10n === "undefined") {
+      return null;
+    }
+    return l10n;
+  }
+
+  // src/internal/GetPhrase.ts
+  function getPhrase(phrase, args) {
+    if (typeof args === "undefined")
+      args = {};
+    var l10n = getL10n();
+    if (l10n == null) {
+      return phrase;
+    }
+    var _p = l10n.Phrases[phrase];
+    var _ri;
+    if (typeof args.__count !== "undefined") {
+      _ri = l10n.ruleEvaluator(args.__count).toString();
+    }
+    if (typeof _p !== "undefined") {
+      if (typeof _ri !== "undefined" && typeof _p.r[_ri] !== "undefined") {
+        phrase = _p.r[_ri];
+      } else {
+        phrase = _p.r["0"];
+      }
+    }
+    return phrase;
+  }
+
+  // src/internal/ReplaceKeywords.ts
+  function replaceKeywords(phrase, args) {
+    var _a, _b;
+    if (typeof args === "undefined") {
+      return phrase;
+    }
+    for (var p in args) {
+      if (args.hasOwnProperty(p)) {
+        phrase = phrase.split("$" + p + "$").join((_b = (_a = args[p]) == null ? void 0 : _a.toString()) != null ? _b : "");
+      }
+    }
+    return phrase;
+  }
+
+  // src/_s.ts
+  function _s(l10nString, formatParameters) {
+    return replaceKeywords(getPhrase(l10nString, formatParameters), formatParameters);
+  }
+
+  // src/L10n.ts
+  if (typeof window !== "undefined") {
+    window._s = _s;
+  }
+  if (typeof global !== "undefined") {
+    global._s = _s;
+  }
+})();
 //# sourceMappingURL=L10n.js.map
